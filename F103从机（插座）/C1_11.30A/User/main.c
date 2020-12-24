4cb2e7f2b48d45a6ce5934f172a52ed94b4555b0
辣椒辣 <dorio7@163.com>
2020-12-24 14:06:08 +0800

    1
    1

--- F103从机（插座）/C1_11.30A/User/main.c
+++ F103从机（插座）/C1_11.30A/User/main.c
@@ -1,25 +1,6 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  fire
-  * @version V1.0
-  * @date    2013-xx-xx
-  * @brief   串口中断接收测试
-  ******************************************************************************
-  * @attention
-  *
-  * 实验平台:野火 F103-MINI STM32 开发板 
-  * 论坛    :http://www.firebbs.cn
-  * 淘宝    :https://fire-stm32.taobao.com
-  *
-  ******************************************************************************
-  */ 
- 
- 
 #include "stm32f10x.h"
 #include "bsp_usart.h"
 #include "bsp_gpio.h"
-
 
 uint32_t E1ST=0;
 u8 SW1_ST[11]={0xFE, 0x08,	0xA1,	0xA1,	0x22,	0x22,	0x01,	0x00,	0x00,	0x00,	0xFF};
@@ -56,20 +37,13 @@ void C1_C(u8 a)
   */
 int main(void)
 {	
-		u8 i;
-	
+
 	SystemInit();
   /*初始化USART 配置模式为 115200 8-N-1，中断接收*/
   USART_Config();
 	USART2_Config();
 	
 	C_GPIO_Config();
-	
-//	/* 发送一个字符串 */
-//	USART_SendData(USART2,0x01);	
-//	USART_SendData(USART1,0x02);
-
-
 
 	USART1_Sned_Char_Buff(SW1_ST,11);
   while(1)
@@ -79,21 +53,9 @@ int main(void)
 			C1_C(1);
 		else
 			C1_C(0);
-		
+	
 		Delay(0X1FFFFf);
-		
-		
-//		USART2_Sned_Char_Buff(SW1_ST,11);
 
-//		for(i=0;i<11;i++)
-//		{
-//		USART_SendData(USART2,SW1_ST[i]);		
-//		/* 等待发送数据寄存器为空 */
-//		while (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);	
-//		}
-
-		
 	}	
 }
 
-/*********************************************END OF FILE**********************/
